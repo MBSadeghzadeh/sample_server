1c6a1ccffa2677da788d4c122efa932966ea14ad
MBSadeghzadeh <mohammadbaghersadeghzadeh@gmail.com>
2024-04-20 20:57:30 +0200

    get port number from input

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sample_server
+
+### Build
+```console
+cmake -S . -B ./build
+cmake --build ./build
+```
+### Run 
+```console
+./build/Server 2456
+```
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <asio.hpp>
 #include <SampleMessage.pb.h>
+#include <thread>
+#include <chrono>
 
 class Session : public std::enable_shared_from_this<Session>
 {
@@ -10,23 +12,42 @@ public:
 
     void start()
     {
-        auto self(shared_from_this());
         do_write();
     }
 
 private:
     void do_write()
     {
-        asio::async_write(socket_, asio::buffer(data_, length),
-                                 [this, self](asio::error_code ec, std::size_t )
-                                 {
-                                     if (!ec)
-                                     {
-                                         do_write();
-                                     }
-                                 });
+        auto self(shared_from_this());
+        TemperatureData td;
+        td.set_temperature(10.5);
+        td.set_device_id("1234");
+        unsigned long milliseconds_since_epoch =
+            std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        td.set_timestamp(milliseconds_since_epoch);
+
+        size_t length = td.ByteSizeLong();
+        char *data_ = new char[8 + length];
+
+        td.SerializeToArray(data_ + 8, length);
+        memcpy(data_, &length, 8);
+
+        asio::async_write(socket_, asio::buffer(data_, 8 + length),
+                          [this, data_, length, self](asio::error_code ec, std::size_t)
+                          {
+                            std::cout << "Write Thread number:" << std::this_thread::get_id() << std::endl;
+                              if (!ec)
+                              {
+                                  std::cout << "wrote:" << length  << std::endl;
+                                  using namespace std::chrono_literals;
+                                  std::this_thread::sleep_for(3s);
+                                  delete []data_;
+                                  do_write();
+                              }
+
+                          });
     }
-    
+
     asio::ip::tcp::socket socket_;
 };
 
@@ -44,6 +65,8 @@ private:
         acceptor_.async_accept(
             [this](asio::error_code ec, asio::ip::tcp::socket socket)
             {
+                std::cout << "Accept Thread number:" << std::this_thread::get_id() << std::endl;
+
                 if (!ec)
                 {
                     std::make_shared<Session>(std::move(socket))->start();
@@ -55,8 +78,25 @@ private:
     asio::ip::tcp::acceptor acceptor_;
 };
 
-int main()
+int main(int argc, char *argv[])
 {
-
+    if (argc > 1)
+    {
+        try
+        {
+            asio::io_context io_context;
+            asio::ip::tcp::endpoint endpoint(asio::ip::tcp::v4(), std::stoi(argv[1]));
+            Server server(io_context, endpoint);
+            io_context.run();
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "Please enter a valid port number." << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "please enter the listening port number for the server: Server <port>" << std::endl;
+    }
     return 0;
 }
